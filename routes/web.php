9d6fb94b3c177a86132660c432b367e83c42140b
Adil-R <adilbutt591@gmail.com>
2024-08-08 17:53:03 +0500

    hey

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::get('/check_new_123', function () {
 Route::get('/check_new_123', function () {
     return view('welcome');
 });
-
+Route::get('/check_new_123', function () {
+    return view('welcome');
+});
